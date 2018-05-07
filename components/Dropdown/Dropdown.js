4697a38b85e26d7f8597cb07bddf7bc998fe6065
Jason O'Neil <jason.oneil@gmail.com>
2018-05-07 12:18:56 +0800

    Add a chevron to control action dropdowns

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import styles from './Dropdown.module.scss';
 import Icon from '../Icon/Icon';
 import defaultIcon from '../../icons/ellipsis.svg';
+import chevronDown from '../../icons/chevron-down.svg';
 import DropdownMenu from './DropdownMenu';
 
 type DropdownState = {
@@ -88,6 +89,12 @@ export default class Dropdown extends React.Component<
             </span>
           )}
           {label && <span className={styles.dropdownLabel}>{label}</span>}
+          {label &&
+            controlAction && (
+              <span className={styles.chevronIcon}>
+                <Icon icon={chevronDown} role="img" title="Open menu" />
+              </span>
+            )}
         </button>
         {this.state.isMenuVisible && this.renderDropdownMenu()}
       </div>
