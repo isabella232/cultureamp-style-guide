64e8d23e3d9a833b99f83da75b7c1aafa4ef9777
Rod Leviton <rod.leviton@cultureamp.com>
2018-10-18 17:14:47 +1100

    fix(guide): reset card component

--- guide/src/components/card/Card.js
+++ guide/src/components/card/Card.js
@@ -2,15 +2,12 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Card.module.scss';
 
-const Card = ({ children, dark, className }) => {
-  const classes = classNames(
-    {
-      [styles.card]: true,
-      [styles.dark]: dark,
-    },
-    className
-  );
-  return <div className={classes}>{children}</div>;
+const Card = ({ children, dark }) => {
+  const className = classNames({
+    [styles.card]: true,
+    [styles.dark]: dark,
+  });
+  return <div className={className}>{children}</div>;
 };
 
 export default Card;
