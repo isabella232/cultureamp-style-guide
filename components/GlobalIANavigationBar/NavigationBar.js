2707ac1c385f9de6c1ecac706c9e4792dd39753a
Jason O'Neil <jason.oneil@gmail.com>
2018-10-27 11:27:08 +0800

    perf(webpack): Upgrade to babel 7, add react-hot-loader plugin
    - The hot-loader babel plugin requires some changes to how NavigationBar works.
      See https://github.com/gaearon/react-hot-loader#checking-element-types
    - And fix some peer dependencies while we're here

--- components/GlobalIANavigationBar/NavigationBar.js
+++ components/GlobalIANavigationBar/NavigationBar.js
@@ -30,10 +30,16 @@ export default class NavigationBar extends React.Component<Props> {
     const links = [];
     const otherChildren = [];
     React.Children.toArray(children).forEach(child => {
-      if (child === false) return;
-      else if (child.type.name == Link.name)
+      if (child === false) {
+        return;
+      }
+      // Because react-hot-loader wraps the type, and uglify changes the type name,
+      // we compare the displayName rather than comparing the type of name directly.
+      if (child.type.displayName == Link.displayName) {
         links.push(((child: any): React.Element<typeof Link>));
-      else otherChildren.push(child);
+      } else {
+        otherChildren.push(child);
+      }
     });
 
     return (
