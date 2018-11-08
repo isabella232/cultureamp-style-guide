3078ab15d92f02aacaa20d64f1165e6e0ad2d4c5
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-11-08 16:57:21 +1100

    Add tooltip and target to global navbar link

--- components/GlobalIANavigationBar/components/Link.js
+++ components/GlobalIANavigationBar/components/Link.js
@@ -4,8 +4,8 @@ import classNames from 'classnames';
 
 import styles from './Link.module.scss';
 import Icon from '../../Icon';
-import iconStyles from '../../Icon/Icon.module.scss';
 import type { SvgAsset } from '../../Icon/Icon.js';
+import Tooltip from './Tooltip.js';
 
 type Props = {|
   icon?: SvgAsset,
@@ -16,47 +16,71 @@ type Props = {|
   id?: string,
   secondary: boolean,
   onClick?: (event: SyntheticMouseEvent<>) => void,
+  tooltip?: string,
+  target: '_self' | '_blank',
 |};
 
-const Link = ({
-  icon,
-  text,
-  href,
-  active,
-  id,
-  onClick,
-  secondary,
-  iconOnly,
-}: Props) => {
-  return (
-    <a
-      className={classNames(styles.link, {
-        [styles.active]: active,
-        [styles.containsText]: typeof text != 'undefined',
-        [styles.secondary]: secondary,
-      })}
-      {...{ href, id, onClick }}
+class Link extends React.Component<Props> {
+  renderLink = () => {
+    const {
+      icon,
+      text,
+      href,
+      active,
+      id,
+      onClick,
+      secondary,
+      iconOnly,
+      target,
+    } = this.props;
+
+    return (
+      <a
+        className={classNames(styles.link, {
+          [styles.active]: active,
+          [styles.containsText]: typeof text != 'undefined',
+          [styles.secondary]: secondary,
+        })}
+        {...{ href, id, onClick, target }}
+      >
+        {icon && (
+          <span className={styles.linkIcon}>
+            <Icon
+              icon={icon}
+              role={iconOnly ? 'img' : 'presentation'}
+              title={iconOnly ? text : undefined}
+            />
+          </span>
+        )}
+        {text &&
+          !(icon && iconOnly) && (
+            <span className={styles.linkText}>{text}</span>
+          )}
+      </a>
+    );
+  };
+
+  renderToolTipLink = () => (
+    <Tooltip
+      hideTooltip={false}
+      tabIndex={null} // link inside takes focus instead
+      tooltip={this.props.tooltip}
     >
-      {icon && (
-        <span className={styles.linkIcon}>
-          <Icon
-            icon={icon}
-            role={iconOnly ? 'img' : 'presentation'}
-            title={iconOnly ? text : undefined}
-          />
-        </span>
-      )}
-      {text &&
-        !(icon && iconOnly) && <span className={styles.linkText}>{text}</span>}
-    </a>
+      {this.renderLink()}
+    </Tooltip>
   );
-};
+
+  render() {
+    return this.props.tooltip ? this.renderToolTipLink() : this.renderLink();
+  }
+}
 
 Link.displayName = 'Link';
 Link.defaultProps = {
   iconOnly: false,
   active: false,
   secondary: false,
+  target: '_self',
 };
 
 export default Link;
